13eda7f7ace675a87a9a61c4d36e2186c4ea56ea
Moniruzzaman Akash <47719046+MnAkash@users.noreply.github.com>
2020-09-27 21:51:49 +0600

    servo update

--- servo/servo.ino
+++ servo/servo.ino
@@ -8,18 +8,30 @@
 }
 
 void loop() {
+
+  servo.write(90);
+  delay(500);
+  
+}
+
+
+
+
+/*
+ * void loop() {
  
   for(i=0;i<180;i++)
   {
     servo.write(i);
-    delay(50);
+    delay(5);
   }
   delay(2000);
 
   for(i=179;i>0;i--)
   {
     servo.write(i);
-    delay(50);
+    delay(5);
   }
   delay(2000);
 }
+*/
